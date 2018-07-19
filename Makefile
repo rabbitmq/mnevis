53fc937c37557864535de74b8a4fdc58d2af1b13
Daniil Fedotov <hairyhum@gmail.com>
2018-07-19 17:20:03 +0100

    RELX Release

--- Makefile
+++ Makefile
@@ -7,10 +7,25 @@ DEPS = ra
 LOCAL_DEPS = mnesia
 TEST_DEPS = proper
 
+NODE_NAME ?= rmns
+RELX_REPLACE_OS_VARS = true
+INITIAL_NODES ?= $(NODE_NAME)
+
+export NODE_NAME
+export RELX_REPLACE_OS_VARS
+export INITIAL_NODES
+
 dep_ra = git https://github.com/rabbitmq/ra master
 
 include erlang.mk
 
+clean:: distclean-relx-rel
+
 repl:
 	erl -pa ebin -pa deps/*/ebin -sname foo
 
+run-cluster:
+	$(verbose) NODE_NAME=rmns1 INITIAL_NODES=rmns1,rmns2,rmns3 $(RELX_OUTPUT_DIR)/$(RELX_REL_NAME)/bin/$(RELX_REL_NAME)$(RELX_REL_EXT) start
+	$(verbose) NODE_NAME=rmns2 INITIAL_NODES=rmns1,rmns2,rmns3 $(RELX_OUTPUT_DIR)/$(RELX_REL_NAME)/bin/$(RELX_REL_NAME)$(RELX_REL_EXT) start
+	$(verbose) NODE_NAME=rmns3 INITIAL_NODES=rmns1,rmns2,rmns3 $(RELX_OUTPUT_DIR)/$(RELX_REL_NAME)/bin/$(RELX_REL_NAME)$(RELX_REL_EXT) start
+	$(verbose) NODE_NAME=rmns2 $(RELX_OUTPUT_DIR)/$(RELX_REL_NAME)/bin/$(RELX_REL_NAME)$(RELX_REL_EXT) attach
--- src/ramnesia_node.erl
+++ src/ramnesia_node.erl
@@ -1,20 +1,36 @@
 -module(ramnesia_node).
 
 -export([start/0, node_id/0]).
-
+-export([make_initial_nodes/1]).
 
 node_id() ->
     {ramnesia_node, node()}.
 
 start() ->
-    Uid = <<"ramnesia_node">>,
     Name = ramnesia_node,
     NodeId = node_id(),
-    Config = #{id => NodeId,
-               uid => Uid,
-               cluster_id => Name,
-               initial_nodes => [NodeId],
-               log_init_args => #{uid => Uid},
-               machine => {module, ramnesia_machine, #{}}},
-    ok = ra:start_node(Config),
-    ok = ra:trigger_election(NodeId).
+    InitialNodes = case application:get_env(ramnesia, initial_nodes) of
+        undefined   -> [NodeId];
+        {ok, Nodes} -> make_initial_nodes(Nodes)
+    end,
+    lists:foreach(fun(N) ->
+        io:format("PING ~n"),
+        net_adm:ping(N)
+    end,
+    InitialNodes),
+    ok = ra:start_node(Name, NodeId, {module, ramnesia_machine, #{}}, InitialNodes)
+    % ,
+    % ok = ra:trigger_election(NodeId)
+    .
+
+make_initial_nodes(Nodes) ->
+    [make_initial_node(Node) || Node <- Nodes].
+
+make_initial_node(Node) ->
+    NodeBin = atom_to_binary(Node, utf8),
+    case string:split(NodeBin, "@", trailing) of
+        [_N, _H] -> Node;
+        [N] ->
+            H = inet_db:gethostname(),
+            binary_to_atom(iolist_to_binary([N, "@", H]), utf8)
+    end.
